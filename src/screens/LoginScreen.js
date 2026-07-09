1f2fbeba8621bf667985dab35fead657eecbd5df
Kauê <kaue_figueiredo.lp@hotmail.com>
2026-07-09 15:53:34 -0300

    feat: create login screen layout

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,9 +1,22 @@
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, TextInput, Pressable } from 'react-native';
 
 export default function LoginScreen() {
     return (
         <View style={styles.container}>
             <Text style={styles.texto}>Login Screen</Text>
+            <TextInput
+                style={styles.Input}
+                placeholder="Email"
+            />
+            <TextInput
+                style={styles.Input}
+                placeholder="Password"
+                secureTextEntry
+            />
+            <Pressable
+                style={styles.botao}>
+                <Text style={styles.textoBotao}>Entrar</Text>
+            </Pressable>
         </View>
     );
     
@@ -20,6 +33,21 @@ const styles = StyleSheet.create({
         color: '#e4e4e4',
         fontSize: 20,
         marginBottom: 10,
+    },
+    Input: {
+        marginBottom: 10,
+        backgroundColor: '#ffffff',
+        padding: 10,
+        borderRadius: 5,
+        width: '80%',
+    },
+    botao: {
+        width: '60%',
+        backgroundColor: '#11c069',
+        padding: 12,
+        borderRadius: 8,
+        marginBottom: 10,
+        alignItems: 'center', 
     }
 
 });
